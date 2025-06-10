1ba08a60511726f7e1b2851e4edce4e74a270fa9
Shravan Asati <dev.shravan@protonmail.com>
2025-06-10 15:39:46 +0530

    extract string unions into their own enum types

--- src/notificationsDTO/types.ts
+++ src/notificationsDTO/types.ts
@@ -1,12 +1,31 @@
 import mongoose, { Types } from "mongoose";
 
+export const NOTIFICATION_TYPE = {
+  MESSAGE: 'message',
+  MENTION: 'mention',
+  LIKE: 'like',
+  REPLY: 'reply',
+} as const;
+
+export type NotificationType = typeof NOTIFICATION_TYPE[keyof typeof NOTIFICATION_TYPE];
+
+export const NOTIFICATION_STATUS = {
+  MESSAGE: 'message',
+  MENTION: 'mention',
+  LIKE: 'like',
+  REPLY: 'reply',
+} as const;
+
+export type NotificationStatus = typeof NOTIFICATION_STATUS[keyof typeof NOTIFICATION_STATUS];
+
+
 export interface INotification {
   sourceUserId: Types.ObjectId; // ID of the user who triggered the notification
   targetUserId: Types.ObjectId; // ID of the user who will receive the notification
-  type: 'message' | 'mention' | 'like' | 'reply'; // Type of notification
+  type: NotificationType; // Type of notification
   title: string; // Title of the notification
   content: string; // Content of the notification
-  status: 'unread' | 'read'; // Status of the notification
+  status: NotificationStatus; // Status of the notification
   createdAt: Date; // Timestamp when the notification was created
   updatedAt: Date; // Timestamp when the notification was last updated
   link: string | null; // Optional link to redirect the user when they click the notification
